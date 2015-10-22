40069cc9f867343ea3a0edbae143edfdebaadc3f
Zachary <zachary@zacharyc.com>
2015-10-21 18:45:31 -0700

    Start laying the framework for a network call

--- hashboard/hashboard/DetailViewController.m
+++ hashboard/hashboard/DetailViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "DetailViewController.h"
+#import "AFHTTPRequestOperation.h"
+#import "AFHTTPRequestOperationManager.h"
 
 @interface DetailViewController ()
 
@@ -36,6 +38,16 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     [self configureView];
+
+    // make network request
+    NSDictionary *configuration = [[NSDictionary alloc] initWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"appSettings" ofType:@"plist"]];
+    AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
+    NSString *urlString = [NSString stringWithFormat:@"https://api.instagram.com/v1/tags/fieldnotes/media/recent?client_id=%@", [configuration objectForKey:@"instagramKey"]];
+    [manager GET:urlString parameters:nil success:^(AFHTTPRequestOperation *operation, id responseObject) {
+        NSLog(@"JSON: %@", responseObject);
+    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+        NSLog(@"Error: %@", error);
+    }];
 }
 
 - (void)didReceiveMemoryWarning {
